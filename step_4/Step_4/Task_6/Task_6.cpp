4439e49138ee50780cba00c7556963700435ba7e
jesper.malmberg <jesper.malmberg86@gmail.com>
2022-01-28 18:43:50 -0600

    added Task 4

--- step_4/Step_4/Task_6/Task_6.cpp
+++ step_4/Step_4/Task_6/Task_6.cpp
@@ -10,11 +10,8 @@
 // Preprocessor directives
 #include <iostream>
 
-
 using namespace std;
-// Prototypes
-void enterData(struct Student);
-void printData(struct Student);
+
 // Constants
 const int QUIZZES = 2;
 const int EXAMS = 2;
@@ -22,16 +19,43 @@ const int NUMBEROFSTUDENTS = 3;
 
 // The datastructure Student holds information on a student
 struct Student {
-	int id;
-	double quizzes[QUIZZES];
-	double exams[EXAMS];
-	double averageScore;
-	char grade;
+	int id;						// The id of the Student
+	double quizzes[QUIZZES];	// An array with 2 quiz results
+	double exams[EXAMS];		// An array with 2 exam results
+	double averageScore;		// The average score
+	char grade;					// Final grade
 
-	double totalNumericScore() {
-
+	// Calculates the average or total score as well as the final grade
+	// Total score(average score) is calculated as follows:
+	// Final exam (exams[1]) = 50%
+	// Midterm exam (exams[0]) = 25%
+	// Average of the two quizzes = 25%
+	// Grade >= 90 = A, 90 >= 80 = B, 80 >= 70 = C, 70 >= 60 = D, <60 = F
+	void totalNumericScore() {
+		double examWorth = exams[1] * 0.5  + exams[0] * 0.25;
+		double quizWorth = (quizzes[0] + quizzes[1]) / 2 * 10 * 0.25;
+		averageScore = examWorth + quizWorth;
+		if (averageScore >= 90) {
+			grade = 'A';
+		}
+		else if (averageScore < 90 && averageScore >= 80) {
+			grade = 'B';
+		}
+		else if (averageScore < 80 && averageScore >= 70) {
+			grade = 'C';
+		}
+		else if (averageScore < 70 && averageScore >= 60) {
+			grade = 'D';
+		}
+		else if (averageScore < 60) {
+			grade = 'F';
+		}
 	}
 };
+
+// Prototypes
+void enterData(Student&);
+void printData(Student);
 
 int main()
 {
@@ -44,37 +68,23 @@ int main()
 	do {
 		system("CLS");			// Clear the console
 
-		
-
-		for (int i = 0; i < NUMBEROFSTUDENTS; i++) {
-			cout << "Enter student number: " << endl;
-			std::cin >> students[i].id;
-			std::cin.clear();
-			std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << "Enter two 10 point quizzes" << endl;
-			std::cin >> students[i].quizzes >> students[i].quizzes;
-			cout << "Enter the midterm and final exam grades. These are 100 point tests" << endl;
-			std::cin >> students[i].exams >> students[i].exams;
+		// Enter the data tabout each student's quizzes and exams
+		for (Student& stu : students) {
+			enterData(stu);
 		}
 
-		for (int i = 0; i < NUMBEROFSTUDENTS; i++) {
-			cout << students[i].exams[1] << endl;
-			cout << students[i].exams[2] << endl;
-
+		// Print all student records
+		for (Student stu : students) {
+			stu.totalNumericScore();
+			printData(stu);
+			cout << endl;
 		}
-		
-		
-
-
-
-
-
-
-
-
 
 		cout << "Enter more students? (Y/N) ";
-		std::cin.get(answer);
+		// Clear the input buffer of errors and old data
+		std::cin.clear();
+		std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin.get(answer);
 		// Clear the input buffer of errors and old data
 		std::cin.clear();
 		std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -83,11 +93,28 @@ int main()
 	return 0;
 }
 
-void enterData(Student Stu) {
-
-
+//-----------------------------------------------------------------------
+// void enterData(Student&)
+// Enter the record of a student
+// Parameter Student is passed by reference to relevant student structure
+//-----------------------------------------------------------------------
+void enterData(Student& stu) {
+	cout << "Enter the student number: ";
+	cin >> stu.id;
+	cout << "Enter two 10 point quizzes" << endl;
+	cin >> stu.quizzes[0] >> stu.quizzes[1];
+	cout << "Enter the midterm and final exam grades. These are 100 point tests" << endl;
+	std::cin >> stu.exams[0] >> stu.exams[1];
 }
-void printData(Student Stu) {
 
+//-----------------------------------------------------------------------
+// void printData(Student)
+// Prints the record of all students scores, total score and grade
+//-----------------------------------------------------------------------
+void printData(Student stu) {
+	cout << "The record for student number : " << stu.id << endl;
+	cout << "The quiz grades are : " << stu.quizzes[0] << " " << stu.quizzes[1] << endl;
+	cout << "The midterm and exam grades are: " << stu.exams[0] << " " << stu.exams[1] << endl;
+	cout << "The total numeric score is : " << stu.averageScore << endl;
+	cout << "and the letter grade assigned is: " << stu.grade << endl;
 }
-
